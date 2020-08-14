ff968a9c57fcbec3cff69ad69a689e2eefb6efc6
Dmitro Szewczuk <dmitro.szewczuk@gmail.com>
2020-08-14 22:37:34 +0200

    A little bit of reducers refactoring

--- src/redux/reducers/flashcard.ts
+++ src/redux/reducers/flashcard.ts
@@ -1,19 +1,19 @@
 import * as actionTypes from '../../constans/actionTypes';
 import { Action, FlashCard } from '../../constans/types';
 
-const initialState = {
-	flashCards: [],
-	getAllErrors: '',
-	createErrors: '',
-};
-
 type State = {
 	flashCards: FlashCard[];
 	getAllErrors: string;
 	createErrors: string;
 };
 
-const reducer = (state: State = initialState, action: Action): State => {
+const initialState: State = {
+	flashCards: [],
+	getAllErrors: '',
+	createErrors: '',
+};
+
+const reducer = (state = initialState, action: Action) => {
 	const { type, payload } = action;
 
 	switch (type) {
--- src/redux/reducers/learning.ts
+++ src/redux/reducers/learning.ts
@@ -2,45 +2,44 @@ import * as actionTypes from '../../constans/actionTypes';
 import { Action, FlashCard } from '../../constans/types';
 import { Answer } from '../../constans/enums';
 
-const initialState = {
-	queue: [],
-	finished: false,
-	currentFlashCard: undefined,
-};
-
 type State = {
 	queue: FlashCard[];
 	finished: boolean;
 	currentFlashCard: FlashCard | undefined;
 };
 
-const reducer = (state: State = initialState, action: Action): State => {
+const initialState: State = {
+	queue: [],
+	finished: false,
+	currentFlashCard: undefined,
+};
+
+const reducer = (state = initialState, action: Action) => {
 	const { type, payload } = action;
 
 	switch (type) {
 		case actionTypes.initLearningType:
-			const allCards = [...payload];
-
-			for (let i = allCards.length - 1; i > 0; i--) {
+			for (let i = payload.length - 1; i > 0; i--) {
 				const j = Math.floor(Math.random() * (i + 1));
-				[allCards[i], allCards[j]] = [allCards[j], allCards[i]];
+				[payload[i], payload[j]] = [payload[j], payload[i]];
 			}
 
-			const current = allCards.shift();
+			const current = payload.shift();
 
-			return { queue: allCards, currentFlashCard: current, finished: false };
+			return { queue: payload, currentFlashCard: current, finished: false };
 		case actionTypes.answerType:
 			const { answer, id } = payload;
 
 			if (answer === Answer.OK) {
-				const queue = state.queue.filter((val) => val._id !== id);
+				const { queue } = state;
 
+				const newQueue = state.queue.filter((val) => val._id !== id);
 				const newFlashCard = queue.shift();
 				const finished = typeof newFlashCard === 'undefined';
 
 				return {
 					...state,
-					queue,
+					queue: newQueue,
 					currentFlashCard: newFlashCard,
 					finished,
 				};
@@ -50,7 +49,6 @@ const reducer = (state: State = initialState, action: Action): State => {
 				if (typeof currentFlashCard !== 'undefined') {
 					queue.push(currentFlashCard);
 				}
-
 				const newFlashCard = queue.shift();
 
 				return {
@@ -58,8 +56,6 @@ const reducer = (state: State = initialState, action: Action): State => {
 					queue,
 					currentFlashCard: newFlashCard,
 				};
-			} else {
-				return state;
 			}
 		default:
 			return state;
